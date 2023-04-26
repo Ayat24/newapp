9d9e64b7818035fd3b52aa1491675286d00f049a
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-04-26 21:10:36 +0200

    add text and icon to item
    Signed-off-by: Ayat24 <126516771+Ayat24@users.noreply.github.com>

--- lib/Add/item.dart
+++ lib/Add/item.dart
@@ -9,6 +9,73 @@ class Item extends StatelessWidget {
   late int CompletedPercent;
   @override
   Widget build(BuildContext context) {
-    return const Container();
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: 3.0,
+            margin: EdgeInsets.only(top: 10),
+            height: 49 * 0.01 * this.CompletedPercent,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 123, 0, 245),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 70,
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(children: [
+                Container(
+                  width: 40,
+                  height: 40,
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 123, 0, 245),
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                  ),
+                  child: const Icon(Icons.assignment, color: Colors.white),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      this.ProjectName,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      "2 days ago",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 10,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
+                  ],
+                ),
+                Expanded(child: Container()),
+                Icon(
+                  Icons.more_vert_outlined,
+                  color: Colors.grey,
+                ),
+              ]),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
