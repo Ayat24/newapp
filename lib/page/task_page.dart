16b775695ae3ab18cdecb29c37fbcaa2a31e089c
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-04-26 18:36:21 +0200

    add app bar
    Signed-off-by: Ayat24 <126516771+Ayat24@users.noreply.github.com>

--- lib/page/task_page.dart
+++ lib/page/task_page.dart
@@ -2,17 +2,17 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
 
-class TasksPage extends StatefulWidget {
-  const TasksPage({
+class TaskPage extends StatefulWidget {
+  const TaskPage({
     Key? key,
     required this.Goback,
   }) : super(key: key);
   final void Function(int) Goback;
   @override
-  State<TasksPage> createState() => _TasksPageState();
+  State<TaskPage> createState() => _TaskPageState();
 }
 
-class _TasksPageState extends State<TasksPage> {
+class _TaskPageState extends State<TaskPage> {
   DateTime _selectedDate = DateTime.now();
   void _onDateChange(DateTime date) {
     this.setState(() {
@@ -21,6 +21,50 @@ class _TasksPageState extends State<TasksPage> {
   }
 
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Container(
+        color: Color.fromRGBO(242, 244, 255, 1),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(25),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  )),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          widget.Goback(1);
+                        },
+                        child: Icon(
+                          Icons.arrow_back_rounded,
+                          color: Colors.black,
+                          size: 30,
+                        ),
+                      ),
+                      Icon(
+                        Icons.search_rounded,
+                        color: Colors.black,
+                        size: 30,
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 25),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
